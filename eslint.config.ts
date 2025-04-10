b0da8d6630c49fa864bd6b2a58160cdd2e97ea5c
Noe TATOUD <noe.tatoud@gmail.com>
2025-04-10 21:54:43 +0200

    feat: configure eslint

---
+++ eslint.config.ts
@@ -0,0 +1,92 @@
+import eslint from '@eslint/js';
+import react from '@eslint-react/eslint-plugin';
+import reactHooks from 'eslint-plugin-react-hooks';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unicorn from 'eslint-plugin-unicorn';
+import tslint from 'typescript-eslint';
+
+export default tslint.config(
+  eslint.configs.recommended,
+  tslint.configs.recommended,
+  react.configs['recommended-typescript'],
+  reactHooks.configs['recommended-latest'],
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      unicorn,
+    },
+    rules: {
+      'unicorn/filename-case': [
+        'error',
+        {
+          case: 'kebabCase',
+        },
+      ],
+    },
+  },
+  {
+    files: ['app/**/*'],
+    rules: {
+      'no-process-env': 'error',
+    },
+  },
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/exports': 'warn',
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            // ext library & side effect imports
+            ['^@?\\w', '^\\u0000'],
+            // {s}css files
+            ['^.+\\.s?css$'],
+            // Lib and hooks
+            ['^@/lib', '^@/hooks'],
+            // static data
+            ['^@/data'],
+            // components
+            ['^@/components', '^@/container'],
+            // zustand store
+            ['^@/store'],
+            // Other imports
+            ['^@/'],
+            // relative paths up until 3 level
+            [
+              '^\\./?$',
+              '^\\.(?!/?$)',
+              '^\\.\\./?$',
+              '^\\.\\.(?!/?$)',
+              '^\\.\\./\\.\\./?$',
+              '^\\.\\./\\.\\.(?!/?$)',
+              '^\\.\\./\\.\\./\\.\\./?$',
+              '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+            ],
+            ['^@/types'],
+            // other that didnt fit in
+            ['^'],
+          ],
+        },
+      ],
+    },
+  }
+);
