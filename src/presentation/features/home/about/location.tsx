a8dba152a9ee2da8c5b5ffcda2cce453b8ee6650
Noe TATOUD <noe.tatoud@gmail.com>
2025-05-02 21:27:15 +0200

    feat: start about location card

--- src/presentation/features/home/about/location.tsx
+++ src/presentation/features/home/about/location.tsx
@@ -1,12 +1,21 @@
 import { CardContent } from '@/presentation/components/ui/card';
 import { BentoCard } from '@/presentation/features/home/about/bento-card';
 
+import GrosHorloge from '/gros-horloge.png';
+
 export function LocationCard() {
   return (
     <BentoCard name="location" className="flex items-center justify-center">
       <CardContent>
-        📍 I currently live in Rouen, France — a beautiful city rich in
-        history... and rain. ☔️
+        <img
+          src={GrosHorloge}
+          alt="Gros Horloge - Rouen"
+          className="mb-6 rounded-lg"
+        />
+        <p>
+          📍 I currently live in Rouen, France — a beautiful city rich in
+          history... and rain. ☔️
+        </p>
       </CardContent>
     </BentoCard>
   );
