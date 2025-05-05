ae82eb221e2bd2b827e9bc58e0ba6d312c9b9492
Noe TATOUD <noe.tatoud@gmail.com>
2025-05-05 23:55:39 +0200

    feat: add small doodles to fill the gaps

--- src/presentation/features/home/about/bento.tsx
+++ src/presentation/features/home/about/bento.tsx
@@ -6,10 +6,25 @@ import { JobCard } from '@/presentation/features/home/about/job';
 import { LocationCard } from '@/presentation/features/home/about/location';
 import { MusicCard } from '@/presentation/features/home/about/music';
 
+import Cosmo from '/cosmo-doodle.png';
+import Planet from '/planet-doodle.png';
+
 export function Bento() {
   return (
     <div className="bento min-h-96">
       <LocationCard />
+      <div
+        style={{ gridArea: 'none' }}
+        className="hidden items-center justify-center xl:flex"
+      >
+        <img src={Planet} alt="Planet" className="h-1/2" />
+      </div>
+      <div
+        style={{ gridArea: 'empty' }}
+        className="hidden items-center justify-center xl:flex"
+      >
+        <img src={Cosmo} alt="Cosmonaut" className="floating h-2/3" />
+      </div>
       <JobCard />
       <ClimbingCard />
       <EyeseightCard />
